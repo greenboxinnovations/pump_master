d6209a134d90de37c509af5a25e98d707c6a9790
GreenBox <greenboxinnovations@gmail.com>
2018-04-11 14:03:57 +0530

    qr_code generator added

---
+++ qr_codes.php
@@ -0,0 +1,41 @@
+<?php
+require 'query/conn.php';
+require 'phpqrcode/qrlib.php';
+
+function generateRand(){
+
+    Global $conn;
+
+    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $charactersLength = strlen($characters);
+    $length = 10;
+    $qr_code = '';
+    for ($i = 0; $i < $length; $i++) {
+        $qr_code .= $characters[rand(0, $charactersLength - 1)];
+    }
+
+    $sql = "SELECT 1 FROM `codes` WHERE `qr_code` = '".$qr_code."' ;";
+    $exe = mysqli_query($conn ,$sql);
+
+    if(mysqli_num_rows($exe) > 0){
+        generateRand();
+    }
+    else{
+    $sql = "INSERT INTO `codes`(`qr_code`)Values('".$qr_code."' );";
+    $exe = mysqli_query($conn ,$sql);
+     return $qr_code;
+    }       
+}
+
+
+for ($i=0; $i < 20; $i++) { 
+
+	$qr =  generateRand();
+
+	$filename= "uploads/".$qr.".png";
+	QRcode::png($qr,$filename, QR_ECLEVEL_L, 15);
+	echo  '<br/>';
+}
+
+?>
+
