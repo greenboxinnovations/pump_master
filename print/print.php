3c106ce005975a3fe9de83acaa3ab1c1a5c92e7b
GreenBox <bhilareakshay1@gmail.com>
2018-07-14 20:32:04 +0530

    printer code added

---
+++ print/print.php
@@ -0,0 +1,112 @@
+<?php
+
+class item {
+	private $vh_no;
+	private $fuel;
+	private $rate;
+	private $ltr;
+	private $amount;
+
+	public function __construct($vh_no = '', $fuel = '',$rate = '',$ltr = '',$amount = '') {
+		$this -> vh_no = $vh_no;
+		$this -> fuel = $fuel;
+		$this -> rate = $rate;
+		$this -> ltr = $ltr;
+		$this -> amount = $amount;
+
+	}
+	
+	public function __toString() {
+		$one = str_pad($this -> vh_no, 15);
+		$two = str_pad($this -> fuel, 8);
+		$three = str_pad($this -> rate, 7);
+		$four = str_pad($this -> ltr,7);
+		$five = str_pad($this -> amount, 0);
+		
+		return "$one$two$three$four$five\n";
+	}
+}
+
+
+require_once(dirname(__FILE__) . "/Escpos.php");
+require '../query/conn.php';
+
+date_default_timezone_set("Asia/Kolkata");
+echo $date = date('l jS F Y h:i:s A');
+$date1 = date('Y-m-d');
+
+if (isset($_GET['trans_id'])) {
+
+	$trans_id = $_GET['trans_id'];
+	
+	/* Start the printer */
+	$logo = new EscposImage("marrakesh.png");
+	// $connector = new WindowsPrintConnector("TM-T81");
+	$connector = new NetworkPrintConnector("192.168.1.108", 9100);
+	$printer = new Escpos($connector);
+
+	/* Print top logo */
+	$printer -> setJustification(Escpos::JUSTIFY_CENTER);
+	$printer -> graphics($logo);
+
+	/* Name of shop */
+	$printer -> selectPrintMode(Escpos::MODE_DOUBLE_WIDTH);
+	$printer -> text("Select Automobiles\n");
+	$printer -> selectPrintMode();
+	$printer -> text("63/10/1 Karve Road, Pune - 411008\n");
+	$printer -> text("GST No 27ADKFS2744J1ZO\n");
+	$printer -> text("Ph No: +91 8329347297");
+	$printer -> feed();		
+	$printer -> setJustification(Escpos::JUSTIFY_LEFT);
+	$printer -> text("------------------------------------------------\n");
+
+
+
+	//header
+	$printer -> setJustification(Escpos::JUSTIFY_LEFT);
+
+	$header = new item("Vehicle No","Fuel","Rate","Ltr","Amount");
+
+	$printer ->text($header);
+	$printer -> text("------------------------------------------------\n");
+	$printer -> feed();
+
+	/* Items */
+	$printer -> setJustification(Escpos::JUSTIFY_LEFT);
+
+	$sql0 = "SELECT a.*,b.car_no_plate FROM  `transactions` a JOIN  `cars` b ON a.car_id=b.car_id WHERE a.trans_id = '".$trans_id."' ;";
+	$result0 = mysqli_query($conn,$sql0);
+
+	while ($row = mysqli_fetch_assoc($result0)) {
+		$vh_no = $row['car_no_plate'];
+		$fuel = $row['fuel'];
+		$ltr = $row['liters'];
+		$rate = $row['rate'];
+		$amount = $row['amount'];
+
+		$line = new item($vh_no,$fuel,$rate,$ltr,$amount);
+	}
+
+	// foreach($items as $item) {
+	$printer -> text($line);
+	// }
+	$printer -> text("------------------------------------------------\n");
+	$printer -> setEmphasis(false);
+		
+
+	/* Footer */
+	$printer -> selectPrintMode();
+	$printer -> feed();
+	$printer -> setJustification(Escpos::JUSTIFY_CENTER);
+	$printer -> text($date);
+	$printer -> feed(2);
+	$printer -> text("Thank you for Visiting Select Automobiles\n");
+	$printer -> feed();
+
+	/* Cut the receipt and open the cash drawer */
+	$printer -> cut();
+	$printer -> pulse();
+	$printer -> close();
+}
+
+?>
