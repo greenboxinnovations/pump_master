a0344b508c2debc3a57062fe67d12e1e039e73dd
Sourabh Jigjinni <sourabhjigjinni@gmail.com>
2018-11-15 13:59:33 +0530

    reprint added

--- print/reprint.php
+++ print/reprint.php
@@ -51,18 +51,18 @@ function intLowHigh($input, $length)
 
 function httpGet($url)
 {
+
     $ch = curl_init();  
  
-    curl_setopt($ch,CURLOPT_URL,$url);
-    curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
-    curl_setopt($ch, CURLOPT_NOBODY, true);    // we don't need body
+    curl_setopt($ch, CURLOPT_URL,$url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 	curl_setopt($ch, CURLOPT_TIMEOUT,1);
 	$output = curl_exec($ch);
 	$httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
- 
     curl_close($ch);
 
     return  array($httpcode, $output);
+
 }
  
 $d = httpGet("http://192.168.0.101/");
@@ -150,6 +150,8 @@ if ((isset($_GET['trans_id']))&&($p)) {
 	$printer -> cut();
 	$printer -> pulse();
 	$printer -> close();
+}else{
+	echo 'Invalid Transaction String Or Printer Not connected';
 }
 
 ?>
