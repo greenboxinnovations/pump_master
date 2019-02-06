06ada30560ef609bb0b2dfe8a4a9e39b5cd8a6de
greenboxinnovations <bhilareakshay1@gmail.com>
2019-02-06 17:39:40 +0530

    optimization

--- receive_videos.php
+++ receive_videos.php
@@ -16,43 +16,43 @@ function myErrorHandler( $errType, $errStr, $errFile, $errLine, $errContext ) {
 ini_set('log_errors', 1);
 ini_set('error_log', 'receive_videos.log');
 error_reporting(E_ALL);
-
+ini_set('max_execution_time', 0);
 set_error_handler('myErrorHandler');
 
 
 if (isset($_POST) ){
 
-	$names  = array();
+	file_put_contents('v_log.txt',print_r($_FILES,true));
+
 	$output = array();
 	$output['success'] = false;
+	$trans_string = "";
 
-	try {
+	try {		
 
-		for ($i=0; $i < sizeof($_FILES['file']['name']); $i++) { 
+		$date = $_POST['date'];
+		$dir  = 'videos/'.$date;
 
-			$dir = $_POST['path'][$i];
-
-			$dir = str_replace('/opt/lampp/htdocs/pump_master/', '', $dir);
-
-			if (!file_exists($dir)) {
-				mkdir($dir);
-			}
-
-			move_uploaded_file($_FILES['file']['tmp_name'][$i], $dir.'/'.$_FILES['file']['name'][$i]);	
-
-			array_push($names, $_FILES['file']['name'][$i]);
-			$output['success'] = true;
+		if (!file_exists($dir)) {
+			mkdir($dir);
 		}
+
+		if(move_uploaded_file($_FILES['file']['tmp_name'], $dir.'/'.$_FILES['file']['name'])){
+			$output['success'] = true;
+			$trans_string =  basename($_FILES['file']['name'], ".mp4");
+		}else{
+			trigger_error('Move upload failed');
+		}			
 		
 	} catch (Exception $e) {
-		array_push($names, $e);
+
 		$output['success'] = false;
 		trigger_error('Test');
 	}
 
-	$output['names']= $names;
+	$output['trans_string']= $trans_string;
 
-	echo json_encode($output);
+	echo json_encode($output,JSON_NUMERIC_CHECK);
 
 }
 
