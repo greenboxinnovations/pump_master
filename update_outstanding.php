834c0060aa6258e8d13816c9abf727a21069de7b
greenboxinnovations <bhilareakshay1@gmail.com>
2019-03-25 13:33:28 +0530

    update outstanding fixed to match outstanding and used balance from standard 1,00,000/-

--- update_outstanding.php
+++ update_outstanding.php
@@ -1,20 +1,45 @@
 <?php
+
 date_default_timezone_set("Asia/Kolkata");
-require_once $_SERVER["DOCUMENT_ROOT"].'/query/conn.php';
 
 if (isset($_GET['ok'])) {
-	$sql = "SELECT `cust_id` FROM `customers` WHERE `cust_post_paid` = 'Y';";	
-	$exe = mysqli_query($conn,$sql);
-	while ( $res = mysqli_fetch_assoc($exe)) {
-		$cust_id = $res['cust_id'];
+	require_once $_SERVER["DOCUMENT_ROOT"].'/query/conn.php';
 
-		$sql2 = "UPDATE `customers` SET `cust_outstanding` = (SELECT SUM(`amount`) FROM `transactions` WHERE `cust_id` = ".$cust_id." AND `billed` = 'N') WHERE `cust_id` = ".$cust_id.";";
+	$sql = "SELECT `cust_id` FROM `customers` WHERE `cust_post_paid` = 'Y';";
+	$exe = mysqli_query($conn, $sql);
+	while($row = mysqli_fetch_assoc($exe)){
 
-		$exe2 = mysqli_query($conn,$sql2);
+			$cust_id = $row['cust_id'];
 
-		echo'Customer Updated';
-		echo'<br/>';
+			$sql1 = "SELECT SUM(`amount`) as amount FROM `transactions` WHERE `cust_id` = ".$cust_id." AND `billed` = 'N';";
+			$exe1 = mysqli_query($conn, $sql1);
+			$row1 = mysqli_fetch_assoc($exe1);
+
+			$amount = $row1['amount'];
+
+
+			$sql2 = "UPDATE `customers` SET `cust_outstanding` = '".$amount."' WHERE `cust_id` = ".$row['cust_id']."  ;";
+			$exe2 = mysqli_query($conn, $sql2);
+
+
+	}
+
+	$sql = "SELECT `cust_id` FROM `customers` WHERE `cust_post_paid` = 'N';";
+	$exe = mysqli_query($conn, $sql);
+	while($row = mysqli_fetch_assoc($exe)){
+
+			$cust_id = $row['cust_id'];
+
+			$sql1 = "SELECT SUM(`amount`) as amount FROM `transactions` WHERE `cust_id` = ".$cust_id." AND `billed` = 'N';";
+			$exe1 = mysqli_query($conn, $sql1);
+			$row1 = mysqli_fetch_assoc($exe1);
+
+			$amount = $row1['amount'];
+			//test
+
+			$sql2 = "UPDATE `customers` SET `cust_balance` = 100000-'".$amount."' WHERE `cust_id` = ".$row['cust_id']."  ;";
+			$exe2 = mysqli_query($conn, $sql2);
+
 	}
 }
-
 ?>
