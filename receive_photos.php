6131b3fbd60af963965ac6b1584da5e3585d0ce4
greenboxinnovations <bhilareakshay1@gmail.com>
2019-07-20 12:03:18 +0530

    receive photos test

--- receive_photos.php
+++ receive_photos.php
@@ -1,6 +1,22 @@
 <?php
-// print_r($_FILES);
-// print_r($_POST);
+date_default_timezone_set("Asia/Kolkata");
+function myErrorHandler( $errType, $errStr, $errFile, $errLine, $errContext ) {
+	$displayErrors 	= ini_get( 'display_errors' );
+	$logErrors 		= ini_get( 'log_errors' );
+	$errorLog 		= ini_get( 'error_log' );
+
+	// if( $displayErrors ) echo $errStr.PHP_EOL;
+	if( $logErrors ) {
+		$message = sprintf('[%s] - (%s, %s) - %s ', date('Y-m-d H:i:s'), $errFile, $errLine ,$errStr);
+		file_put_contents( $errorLog, $message.PHP_EOL, FILE_APPEND );
+	}
+}
+
+ini_set('log_errors', 1);
+ini_set('error_log', 'receive_photos.log');
+error_reporting(E_ALL);
+ini_set('max_execution_time', 0);
+set_error_handler('myErrorHandler');
 
 if (isset($_POST) ){
 	$output = array();
@@ -8,6 +24,8 @@ if (isset($_POST) ){
 	for ($i=0; $i < sizeof($_FILES['file']['name']); $i++) { 
 
 		$dir = $_POST['path'][$i];
+
+		trigger_error($dir);
 
 		$dir = str_replace('/opt/lampp/htdocs/pump_master/', '', $dir);
 
